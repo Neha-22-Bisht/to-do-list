67a7b4d33a8b326fd7ca9c21886ea74713e14944
Neha Bisht <65558894+Neha-22-Bisht@users.noreply.github.com>
2025-04-21 17:09:13 -0700

    commit by another dev
    desc

--- to-do-list/to-do-list/AppDelegate.swift
+++ to-do-list/to-do-list/AppDelegate.swift
@@ -14,6 +14,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        // this is an edit by another developer
         return true
     }
 
